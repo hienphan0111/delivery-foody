14afa990cfef449fe1d90ef08a3b35f012e739ae
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-08-10 14:28:41 +0700

    Add home screen

--- app/Home.js
+++ app/Home.js
@@ -1,9 +1,9 @@
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import { View, Text, TextInput, SafeAreaView, Image } from 'react-native';
 import { useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
-  userIcon,
-  CheronDownIcon,
+  UserIcon,
+  ChevronDownIcon,
   SearchIcon,
   AdjustmentsIcon,
 } from 'react-native-heroicons/outline';
@@ -21,24 +21,37 @@ const Home = () => {
   });
 
   return (
-    <SafeAreaView>
-      <Text>
+    <SafeAreaView className='bg-white pt-5'>
+      <Text className='w-full'>
         {/* Header */}
-        <View className='flex-row gap-3 items-center mx-4'>
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
           <Image
             source={{
               uri: 'https://links.papereact.com/wru',
             }}
             className='h-7 bg-gray-300 p-4 rounded-full'
           />
-          <View>
+          <View className='flex-1'>
             <Text className='font-bold text-gray-400 text-xs'>Deliver Now</Text>
             <Text className='font-bold text-xl'>
               Current Location
-              <CheronDownIcon className='h-5 w-5 text-gray-400' />
+              <ChevronDownIcon size={20} color="#00CCBB" />
             </Text>
           </View>
+
+          <UserIcon size={35} color="#00CCBB" />
         </View>
+
+        {/* Serach */}
+
+      <View>
+        <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
+          <SearchIcon />
+
+          {/* <TextInput placeholder='search' keyboardType='default' />
+          <AdjustmentsIcon color='#00CCBB' /> */}
+        </View>
+      </View>
       </Text>
     </SafeAreaView>
   );
